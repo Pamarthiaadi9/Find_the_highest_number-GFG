ffd00dc5ddb7dc4957b78fc7b66cef8d139e0d58
Aadi Siva Kartheek Pamarthi <105631285+Pamarthiaadi9@users.noreply.github.com>
2024-05-18 22:09:46 +0530

    Create highestnum.py

---
+++ highestnum.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def findPeakElement(self, a: List[int]) -> int:
+        lo, hi = 0, len(a) - 1
+        ans = -1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            ans = max(ans, a[mid])
+            if mid == 0:
+                lo = mid + 1
+            elif mid == len(a) - 1:
+                hi = mid - 1
+            elif a[mid] < a[mid + 1]:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+        return ans
